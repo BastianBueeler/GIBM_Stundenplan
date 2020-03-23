0c1248eb4117bbc56a50864c4c28b34cab9b9242
Bastian Bueeler <bastian.bueeler@gmail.com>
2020-03-23 14:23:31 +0100

    LocalStorage hinzugefügt

---
+++ localStorage.js
@@ -0,0 +1,15 @@
+function setLocalstorageJobId(jobId) {
+    localStorage.setItem("jobId", jobId);
+}
+
+function getLocalstorageJobId() {
+    return localStorage.getItem("jobId");
+}
+
+function setLocalstorageKlassenId(klassenId) {
+    localStorage.setItem("klassenId", klassenId);
+}
+
+function getLocalstorageKlassenId() {
+    return localStorage.getItem("klassenId");
+}
