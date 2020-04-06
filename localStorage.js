b71844b36339e6043ca6fac42851efcf4e21bc99
Bastian Bueeler <bastian.bueeler@gmail.com>
2020-04-06 15:38:04 +0200

    Kommentäre hinzugefügt

--- localStorage.js
+++ localStorage.js
@@ -1,15 +1,19 @@
+//Setzt im LocalStorage die jobId
 function setLocalstorageJobId(jobId) {
     localStorage.setItem("jobId", jobId);
 }
 
+//Gibt den Wert der jobId, welcher im LocalStorage ist, zurueck
 function getLocalstorageJobId() {
     return localStorage.getItem("jobId");
 }
 
+//Setzt im LocalStorage die klassenId
 function setLocalstorageKlassenId(klassenId) {
     localStorage.setItem("klassenId", klassenId);
 }
 
+//Gibt den Wert der klassenId, welcher im LocalStorage ist, zurueck
 function getLocalstorageKlassenId() {
     return localStorage.getItem("klassenId");
 }
--- mainScript.js
+++ mainScript.js
@@ -1,5 +1,6 @@
 $(function () {
 
+    //Erstellen der Objekte in Variablen
     var jobSelect = '#jobSelect';
     var klassenSelect = '#klassenSelect';
     var wochenDisplay = '#wochenDisplay';
@@ -8,13 +9,19 @@ $(function () {
     var buttonVorwaerts = '#buttonRechts';
     var informationsMeldung = '#informationsMeldung';
     var wochenAuswahl = '#wochenAuswahl';
+
+    //Daten aus dem Localstorage laden
     var jobIdLocalstorage = getLocalstorageJobId();
     var klassenIdLocalstorage = getLocalstorageKlassenId()
+
+    //Datumsobjekt erstellen und in Wochenformat umwandeln
     var datum = moment();
     var datumWoche = moment(datum).format('WW-GGGG');
 
+    //Funktion mit allen ActionListener
     function addListeners() {
         $(jobSelect).on('change', function () {
+            //Ueberpruefung auf Inhalt von Jobselect und danach loadKlassenSelect aufrufen, sowie Wert in Localstorage speichern.
             if ($("#jobSelect :selected").val() != "Wählen Sie Ihren Beruf") {
                 loadKlassenSelect();
                 setLocalstorageJobId($("#jobSelect :selected").val());
@@ -22,6 +29,7 @@ $(function () {
         })
 
         $(klassenSelect).on('change', function () {
+            //Ueberpruefung auf Inhalt von Klassenselect und danach loadTable aufrufen, sowie Wert in Localstorage speichern.
             if ($("#klassenSelect :selected").val() != "Wählen Sie Ihren Beruf") {
                 setLocalstorageKlassenId($("#klassenSelect :selected").val());
                 loadTable();
@@ -29,22 +37,27 @@ $(function () {
         })
 
         $(buttonZurueck).on('click', function () {
+            //Vom Datum eine Woche abziehen, es neu in datumWoche abspeichern
             datum = moment(datum).subtract(1, 'w');
             datumWoche = moment(datum).format('WW-GGGG');
 
+            //WochenDisplay aktualisieren und loadTable aufrufen
             $(wochenDisplay).text(moment(datum).format('WW-GGGG'));
             loadTable();
         })
 
         $(buttonVorwaerts).on('click', function () {
+            //Zum Datum eine Woche addieren und es neu in datumWoche abspeichern
             datum = moment(datum).add(1, 'w');
             datumWoche = moment(datum).format('WW-GGGG');
 
+            //WochenDisplay aktualisieren und loadTable aufrufen
             $(wochenDisplay).text(moment(datum).format('WW-GGGG'));
             loadTable();
         })
     }
 
+    //Animiert das bekommene Element
     function doAnimation(element) {
         $(element).animate({
             opacity: '0.2'
@@ -55,10 +68,13 @@ $(function () {
     }
 
     function loadJobSelect() {
+        //API Abfrage
         $.getJSON('https://sandbox.gibm.ch/berufe.php')
+            //Bei Fehler eine Meldung ausgeben.
             .fail(function () {
                 alert("Verbindung zum Server fehlgeschlagen!");
             })
+            //Ansonsten die Daten in JobSelect abfuellen
             .done(function (data) {
                 $(jobSelect)
                     .empty()
@@ -68,7 +84,8 @@ $(function () {
                         $('<option value="' + jobSelector.beruf_id + '">' + jobSelector.beruf_name + '</option>').appendTo($(jobSelect));
                     }
                 })
-                if (jobIdLocalstorage != null && jobIdLocalstorage != "") {
+                //Falls schon Daten im LocalStorage sind, werden diese eingetragen
+                if (jobIdLocalstorage != null) {
                     $(jobSelect + " option[value='" + jobIdLocalstorage + "']").attr("selected", 'true');
                     loadKlassenSelect();
                 }
@@ -76,7 +93,9 @@ $(function () {
     }
 
     function loadKlassenSelect() {
+        //Abfrage des Inhalts des jobSelects
         if ($("#jobSelect :selected").val() == 0) {
+            //verstecken der Elemente
             $(klassenSelect).hide();
             $(informationsMeldung).removeClass("d-flex");
             $(wochenAuswahl).removeClass("d-flex");
@@ -86,10 +105,13 @@ $(function () {
         } else {
             $(klassenSelect).show();
             var jobID = $("#jobSelect :selected").val();
+            //Anhand des Inhalts mit der API die Daten fürs KlassenSelect abfragen
             $.getJSON('https://sandbox.gibm.ch/klassen.php?beruf_id=' + jobID)
+                //Bei Fehler, Meldung ausgeben
                 .fail(function () {
                     alert("Verbindung zum Server fehlgeschlagen!");
                 })
+                //Ansonsten die Daten ins klassenSelect abfuellen.
                 .done(function (data) {
                     $(klassenSelect)
                         .empty()
@@ -97,7 +119,8 @@ $(function () {
                     $.each(data, function (i, klassenSelector) {
                         $('<option value="' + klassenSelector.klasse_id + '">' + klassenSelector.klasse_longname + '</option>').appendTo($(klassenSelect));
                     })
-                    if (klassenIdLocalstorage != null && klassenIdLocalstorage != "") {
+                    //Falls schon Daten im LocalStorage sind, werden diese eingetragen
+                    if (klassenIdLocalstorage != null) {
                         $(klassenSelect + " option[value='" + klassenIdLocalstorage + "']").attr("selected", 'true');
                         loadTable();
                     }
@@ -106,28 +129,37 @@ $(function () {
     }
 
     function loadTable() {
+        //Abfrage des Inhalts des klassenSelect
         if ($("#klassenSelect :selected").val() == 0) {
+            //verstecken der Elemente
             $(informationsMeldung).removeClass("d-flex");
             $(wochenAuswahl).removeClass("d-flex");
             $(informationsMeldung).hide();
             $(wochenAuswahl).hide();
             $(stundenplanContainer).empty();
         } else {
+            //anzeigen der richtigen Elemente
             $(wochenAuswahl).addClass("d-flex");
             $(wochenAuswahl).show();
             $(informationsMeldung).removeClass("d-flex");
             $(informationsMeldung).hide();
             var klassenID = $("#klassenSelect :selected").val();
+            //API abfrage anhand von klassenID und datumWoche
             $.getJSON('http://sandbox.gibm.ch/tafel.php?klasse_id=' + klassenID + '&woche=' + datumWoche)
+                //Bei Fehler wird meldung ausgegeben
                 .fail(function () {
                     alert("Verbindung zum Server fehlgeschlagen!");
                 })
+                //Starten mit dem Aufbau der Tabelle
                 .done(function (data) {
                     $(stundenplanContainer).empty();
+                    //Wenn die Datenlänge nicht 0 ist wird die Tabelle aufgebaut, ansonsten wird eine Informationsmeldung angezeigt.
                     if (data.length != 0) {
+                        //Header der Tabelle
                         $(stundenplanContainer).append(
                             '<thead><tr><th scope="col">Datum</th><th scope="col">Tag</th><th scope="col">Von</th><th scope="col">Bis</th><th scope="col">Lehrer</th><th scope="col">Fach</th><th scope="col">Raum</th></tr></thead>'
                         );
+                        //Alle Eintraege durchlaufen und in Tabelle abfuellen
                         $.each(data, function (i, objectStundenplan) {
                             $(stundenplanContainer).append(
                                 '<tr>' +
@@ -157,30 +189,38 @@ $(function () {
                                 '</tr>'
                             );
                         });
+                        //Animation wird gemacht, damit eine Veraenderung klar ersichtlich ist
                         doAnimation(stundenplanContainer);
                     } else {
+                        //Anzeigen der Information, falls keine Daten gefunden wurden
                         $(informationsMeldung).addClass("d-flex");
                         $(informationsMeldung).show();
+
+                        //Animation wird gemacht, damit eine Veraenderung klar ersichtlich ist
                         doAnimation(informationsMeldung);
                     }
                 });
         }
     }
 
-
+    //Funktion zum Starten der ganzen Applikation
     function startApplication() {
+        //Versteckt Elemente
         $(klassenSelect).hide();
         $(informationsMeldung).removeClass("d-flex");
         $(wochenAuswahl).removeClass("d-flex");
         $(informationsMeldung).hide();
         $(wochenAuswahl).hide();
 
+        //Setzt aktuelles Datum aufs wochenDisplay
         $(wochenDisplay).text(datumWoche);
 
+        //ruft die ersten Methoden auf
         addListeners();
         loadJobSelect();
 
     }
 
+    //Methodenaufruf zum Starten der Applikation
     startApplication();
 });
